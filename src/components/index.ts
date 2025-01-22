9e4ef9c0b80d94d79d8f406f361590431bf313ce
Alexis Herrera <desert5@outlook.es>
2025-01-22 20:51:12 -0300

    Errors fixed and code refactor

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,3 @@
-import SocialBanner from "./SocialBanner.astro";
-import Header from "./Header.astro";
-import { Navigation } from "./Navigation";
-
+export * from "./Navigation";
 export * from "./global";
-export { SocialBanner, Header, Navigation };
+export * from "./home";
