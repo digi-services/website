3b0f14d96586c6a0979c766d2ef8e7c7203241a7
Daniel <d.i.marinmora@gmail.com>
2025-01-27 22:48:25 -0300

    feat: Add Contact component and Footer component

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -3,5 +3,7 @@ import Hero from "./Hero.astro";
 import Services from "./Services.astro";
 import Plans from "./Plans.astro";
 import Portfolio from "./Portfolio.astro";
+import Contact from "./Contact.astro";
+import Footer from "./Footer.astro";
 
-export { SocialBanner, Hero, Services, Plans, Portfolio };
+export { SocialBanner, Hero, Services, Plans, Portfolio, Contact, Footer };
