15c4a12941b68cc6547da0912c8c38e6bd0a3c87
Alexis Herrera <desert5@outlook.es>
2025-03-05 08:56:42 -0300

    Packages updated, tailwindcss using vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
-
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    server: {
-      watch: {
-        usePolling: true,
-      },
-    },
+    plugins: [tailwindcss()]
   },
-  integrations: [tailwind(), react()],
+
+  integrations: [react()]
 });
